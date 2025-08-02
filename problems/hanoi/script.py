af8f7d3dc15320c2680a1c4f687bcb994809e203
PrzemyslawSwiderski <przemekzgierz7@gmail.com>
2025-08-03 01:06:41 +0200

    Hanoi Solution optimization

--- problems/hanoi/script.py
+++ problems/hanoi/script.py
@@ -1,6 +1,5 @@
 #!/bin/python3
 from collections import deque
-from copy import deepcopy
 
 
 # https://www.hackerrank.com/challenges/gena/problem?isFullScreen=true
@@ -10,63 +9,70 @@ from copy import deepcopy
 # The function accepts INTEGER_ARRAY posts as parameter.
 #
 
-def hanoi(posts):
-    disks_count = len(posts)
+def no_smaller_disks_on_target(current_disks, target_pole, disk_i):
+    while disk_i >= 0:
+        disk_i -= 1
+        if current_disks[disk_i] == target_pole:
+            return False
+
+    return True
+
+
+def hanoi(disks_arr):
     to_process = deque()
     visited = set()
 
-    disks_by_pole = disk_map(posts)
-
-    init_poles = deepcopy(disks_by_pole)
-    to_process.append({"data": init_poles, "steps": 0})
-    visited.add(hashable(init_poles))
-    # print(disks_by_pole)
+    to_process.append({"data": disks_arr, "steps": 0})
+    visited.add(hashable(disks_arr))
 
     while True:
         current = to_process.popleft()
-        current_data = current["data"]
+        current_disks = current["data"]
 
-        if is_solved(current_data, disks_count):
+        if is_solved(current_disks):
             return current["steps"]
-        for i in range(1, 5):
-            for j in range(1, 5):
-                if j != i and current_data[i]:
-                    if (current_data[j] and current_data[j][0] > current_data[i][0]) or not current_data[j]:
-                        to_add = deepcopy(current_data)
-                        to_move = to_add[i].popleft()
-                        to_add[j].appendleft(to_move)
-                        to_add_hash = hashable(to_add)
-                        if to_add_hash not in visited:
-                            visited.add(to_add_hash)
-                            to_process.append({"data": to_add, "steps": current["steps"] + 1})
+
+        max_non_set_idx = find_max_idx(current_disks)
+        for disk_i in range(max_non_set_idx + 1):
+            current_pole = current_disks[disk_i]
+            for target_pole in range(1, 5):
+                if target_pole != current_pole and not_blocked(current_disks, current_pole, target_pole, disk_i):
+                    to_add = current_disks.copy()
+                    to_add[disk_i] = target_pole
+                    to_add_hash = hashable(to_add)
+                    if to_add_hash not in visited:
+                        visited.add(to_add_hash)
+                        to_process.append({"data": to_add, "steps": current["steps"] + 1})
 
 
-def disk_map(arr):
-    disks = {}
-    for i in range(1, 5):
-        disks[i] = deque()
-    for idx, e in enumerate(arr):
-        disk_num = idx + 1
-        disks[e].append(disk_num)
-    return disks
+def find_max_idx(disks):
+    disk_idx = len(disks) - 1
+    while disk_idx >= 0 and disks[disk_idx] == 1:
+        disk_idx -= 1
+    return disk_idx
 
 
-def is_solved(disks, disks_count):
-    if len(disks[1]) == disks_count:
-        return True
-    else:
-        return False
+def not_blocked(current_disks, current_pole, target_pole, disk_i):
+    while disk_i > 0:
+        disk_i -= 1
+        if (current_disks[disk_i] == current_pole  # other does not block disk at the same pole
+                or current_disks[disk_i] == target_pole  # there is no smaller disk on target
+        ):
+            return False
+
+    return True
 
 
-def hashable(input_dict):
-    output = ()
-    for it in input_dict.items():
-        output += (it[0], tuple(it[1]),)
-    return output
+def is_solved(disks):
+    return all(x == 1 for x in disks)
+
+
+def hashable(input_obj):
+    return str(input_obj)
 
 
 if __name__ == '__main__':
-    # loc = [1, 4, 1] # expected 3
+    # loc = [1, 4, 1]  # expected 3
     loc = [1, 3, 3]  # expected 5
 
     res = hanoi(loc)
