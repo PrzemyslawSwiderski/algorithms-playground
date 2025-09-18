f8060cdbe47fc8adb58917476b439410611ef9c2
Przemyslaw Swiderski <przemyslaw.swiderski7@gmail.com>
2025-09-18 22:57:28 +0200

    bear-and-steady-gene solution

---
+++ problems/bear-and-steady-gene/script.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+from collections import Counter
+
+
+# https://www.hackerrank.com/challenges/bear-and-steady-gene/problem
+# Complete the 'steady_gene' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING gene as parameter.
+#
+
+def steady_gene(genes):
+    genes_size = len(genes)
+    goal = genes_size // 4
+    curr = Counter(genes)
+    ans = genes_size
+    left_ptr = 0
+    right_ptr = 0
+    while right_ptr < genes_size:
+        curr[genes[right_ptr]] -= 1
+        while left_ptr < genes_size and max(curr.values()) <= goal:
+            ans = min(ans, right_ptr - left_ptr + 1)
+            curr[genes[left_ptr]] += 1
+            left_ptr += 1
+        right_ptr += 1
+
+    return ans
+
+
+if __name__ == '__main__':
+    # gene = "GAAATAAA" # res = 5
+    gene = "TGATGCCGTCCCCTCAACTTGAGTGCTCCTAATGCGTTGC"  # res = 5
+
+    result = steady_gene(gene)
+
+    print(result)
