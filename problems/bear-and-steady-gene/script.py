ff72747939de8f43594f3b012c2ff9eafb7ce16c
Przemyslaw Swiderski <przemyslaw.swiderski7@gmail.com>
2025-09-21 17:37:18 +0200

    bear-and-steady-gene improvements

--- problems/bear-and-steady-gene/script.py
+++ problems/bear-and-steady-gene/script.py
@@ -17,20 +17,28 @@ def steady_gene(genes):
     ans = genes_size
     left_ptr = 0
     right_ptr = 0
+    min_left_ptr = 0
+    min_right_ptr = 0
     while right_ptr < genes_size:
         curr[genes[right_ptr]] -= 1
         while left_ptr < genes_size and max(curr.values()) <= goal:
-            ans = min(ans, right_ptr - left_ptr + 1)
+            window_size = right_ptr - left_ptr + 1
+            if window_size < ans:
+                ans = window_size
+                min_left_ptr = left_ptr
+                min_right_ptr = right_ptr
             curr[genes[left_ptr]] += 1
             left_ptr += 1
         right_ptr += 1
 
+    print(f"{min_left_ptr=} -> {min_right_ptr=}")
     return ans
 
 
 if __name__ == '__main__':
     # gene = "GAAATAAA" # res = 5
-    gene = "TGATGCCGTCCCCTCAACTTGAGTGCTCCTAATGCGTTGC"  # res = 5
+    gene = "TTGTTCCGTTTT" # res = 8
+    # gene = "TGATGCCGTCCCCTCAACTTGAGTGCTCCTAATGCGTTGC"  # res = 5
 
     result = steady_gene(gene)
 
