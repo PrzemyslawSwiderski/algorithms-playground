eb283b96271d16ad0a2ed089a28dbfca5e5873e1
Przemyslaw Swiderski <przemyslaw.swiderski7@gmail.com>
2025-10-29 00:44:38 +0100

    Migrated to uv Python Plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,40 +1,36 @@
-import com.pswidersk.gradle.python.VenvTask
+import com.pswidersk.gradle.python.uv.UvTask
 
 plugins {
-    id("com.pswidersk.python-plugin") version "2.7.2"
-}
-
-pythonPlugin {
-    pythonVersion = "3.12.2"
-    condaInstaller = "Miniconda3"
-    condaVersion = "py312_24.1.2-0"
+    id("com.pswidersk.python-uv-plugin") version "0.2.0"
 }
 
 tasks {
 
-    register<VenvTask>("condaInfo") {
-        venvExec = "conda"
-        args = listOf("info")
+    register<UvTask>("uvInfo") {
+        args = listOf("help")
     }
 
-    val condaInstall by registering(VenvTask::class) {
+    val uvEnvInstall by registering(UvTask::class) {
+        args = listOf("venv")
+    }
+
+    val uvInstall by registering(UvTask::class) {
         val requirementsFile = projectDir.resolve("requirements.txt").path
-        venvExec = "conda"
-        args = listOf("install", "--file", requirementsFile)
-        outputs.file(requirementsFile)
+        args = listOf("pip", "install", "-r", requirementsFile)
+        dependsOn(uvEnvInstall)
     }
 
     projectDir.resolve("problems").listFiles().forEach { file ->
-        register<VenvTask>("run-${file.name}") {
-            args = listOf(file.resolve("script.py").path)
+        register<UvTask>("run-${file.name}") {
+            group = "algos"
+            args = listOf("run", file.resolve("script.py").path)
             val input = file.resolve("input.txt")
             if (input.exists()) {
-                inputFile.set(file.resolve("input.txt"))
+                standardInput = input.inputStream()
             }
             environment = mapOf("OUTPUT_PATH" to file.resolve("output.txt"))
-            dependsOn(condaInstall)
+            dependsOn(uvInstall)
         }
     }
-
 
 }
